378e6c487b6293065c7f91c47f98825b9246d671
adrianagraybill <adriana.graybill@gmail.com>
2019-07-16 22:54:50 -0700

    done

--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -12,6 +12,27 @@ class LinkedList {
     this.head = null;
   }
 
+  length() {
+    let count = 0;
+    let current = this.head;
+    while(current.next) {
+      count ++;
+      current = current.next;
+    }
+    return count;
+  }
+
+  isLongerThan(list) {
+    return (this.length() > list.length() ?
+      true : false );
+  }
+
+  // compare(list1, list2) {
+  //   return (list1.length() > list2.length() ?
+  //     list1 :
+  //     list2 );
+  // }
+
   insert(value) {
     this.head = new Node(value, this.head);
   }
@@ -85,4 +106,35 @@ class LinkedList {
   }
 }
 
-module.exports = LinkedList;
+function mergeLists(list1, list2) {
+
+  let longerList;
+  let shorterList;
+
+  if (list1.isLongerThan(list2)){
+    longerList = list1;
+    shorterList = list2;
+
+  } else {
+    longerList = list2;
+    shorterList = list1;
+  }
+
+  let shortCurrent = shorterList.head;
+  let longCurrent = longerList.head;
+
+  while (shortCurrent) {
+
+    let shortNext = shortCurrent.next;
+    let longNext = longCurrent.next;
+
+    longCurrent.next = shortCurrent;
+    shortCurrent.next = longNext;
+
+    shortCurrent = shortNext;
+    longCurrent = longNext;
+  }
+  return longerList.head;
+}
+
+module.exports = {LinkedList, mergeLists};
