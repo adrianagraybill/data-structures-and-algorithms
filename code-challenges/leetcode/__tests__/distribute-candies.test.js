a95fcc94605ed76420027a4bb32a2c3c3f710eba
adrianagraybill <adriana.graybill@gmail.com>
2019-12-18 20:44:33 -0800

    successfully solved two-sum challenge on leetcode

--- code-challenges/leetcode/__tests__/distribute-candies.test.js
+++ code-challenges/leetcode/__tests__/distribute-candies.test.js
@@ -6,4 +6,4 @@ describe('Count the candies', () => {
   it('can do stuff', () => {
     expect(candies([1,1,2,2,3,3])).toEqual(3);
   });
-}); 
+});
--- code-challenges/leetcode/distribute-candies/distribute-candies.js
+++ code-challenges/leetcode/distribute-candies/distribute-candies.js
@@ -2,18 +2,18 @@
 
 function distributeCandies(candies) {
   let obj = {};
-   let objKeys = 0;
+  let objKeys = 0;
   for(let i = 0; i < candies.length; i++ ){
-      if(!obj[candies[i]]){
-          obj[candies[i]] = 1; 
-          objKeys +=1
-      }
-      else{
-          obj[candies[i]] += 1;
-      }
+    if(!obj[candies[i]]){
+      obj[candies[i]] = 1;
+      objKeys +=1;
+    }
+    else{
+      obj[candies[i]] += 1;
+    }
   }
-   let halfOfCandies = candies.length /2 
-   return objKeys > halfOfCandies ? halfOfCandies : objKeys;
+  let halfOfCandies = candies.length /2;
+  return objKeys > halfOfCandies ? halfOfCandies : objKeys;
 }
 
 module.exports = distributeCandies;
