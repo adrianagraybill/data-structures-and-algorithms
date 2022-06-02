9484e194edfa1d8112a9597d7f06b7033549171e
Adriana Graybill <adriana.graybill@gmail.com>
2022-06-01 20:42:59 -0700

    refactored original solution

--- code-challenges/leetcode/two-sum/two-sum.js
+++ code-challenges/leetcode/two-sum/two-sum.js
@@ -1,17 +1,14 @@
 'use strict';
 
-var twoSum = function(nums, target) {
-  let targetOne;
-  let targetTwo;
-  for(let i = 0; i<nums.length; i++){
-    targetOne=nums[i];
-    for(let j = 1; j<nums.length; j++){
-      targetTwo=nums[j];
-      if ((targetOne+targetTwo)===target&&(i!==j)){
-        return [i, j];
+const twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if ( i !== j && nums[i] + nums[j] === target ) {
+        return [i,j];
       }
     }
   }
 };
 
 module.exports = twoSum;
+
